42b7a27e0e2eb05c8254b36ac0adf9aebc20cb01
Thomas Beirne <thomas.beirne@gmail.com>
2016-08-06 16:22:08 -0700

    Tidy up whitespace

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,28 +1,30 @@
 ﻿using UnityEngine;
 
-public class Shooting : MonoBehaviour {
-	public GameObject BulletObj;
-	private GameObject PlayerShip;
+public class Shooting : MonoBehaviour
+{
+    public GameObject BulletObj;
+    private GameObject PlayerShip;
 
-	// Use this for initialization
-	void Start () {
-		PlayerShip = transform.parent.parent.gameObject;
-		BulletObj = Resources.Load<GameObject>("Prefabs/Bullet");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
-			var bullet = (GameObject)Instantiate (BulletObj, gameObject.transform.position, gameObject.transform.rotation);
+    void Start()
+    {
+        PlayerShip = transform.parent.parent.gameObject;
+        BulletObj = Resources.Load<GameObject>("Prefabs/Bullet");
+    }
 
-			// Give the bullet the ship's velocity so it goes faster if the player is moving
-			var shipVelocity = PlayerShip.GetComponent<PlayerMovement>().CurrentVelocity;
-			bullet.GetComponent<Bullet>().SetPlayerShipVelocity(shipVelocity);
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var bullet = (GameObject)Instantiate(BulletObj, gameObject.transform.position, gameObject.transform.rotation);
 
-			// Don't allow bullets to collide with the player
-			var bulletCollider = bullet.GetComponent<Collider2D>();
-			var shipCollider = PlayerShip.GetComponent<Collider2D>();
-			Physics2D.IgnoreCollision(bulletCollider, shipCollider);
-		}
-	}
+            // Give the bullet the ship's velocity so it goes faster if the player is moving
+            var shipVelocity = PlayerShip.GetComponent<PlayerMovement>().CurrentVelocity;
+            bullet.GetComponent<Bullet>().SetPlayerShipVelocity(shipVelocity);
+
+            // Don't allow bullets to collide with the player
+            var bulletCollider = bullet.GetComponent<Collider2D>();
+            var shipCollider = PlayerShip.GetComponent<Collider2D>();
+            Physics2D.IgnoreCollision(bulletCollider, shipCollider);
+        }
+    }
 }
