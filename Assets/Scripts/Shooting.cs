14fe81d48210fcbda443ff4f37c357093b46c890
Thomas Beirne <thomas.beirne@gmail.com>
2016-08-06 16:14:41 -0700

    Don't collide with your own bullets
    This also switches to shooting a single bullet with every press of the shooting button because THAT'S HOW THE ORIGINAL WORKS STEVE TRY TO HAVE SOME RESPECT.

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -3,30 +3,23 @@
 public class Shooting : MonoBehaviour {
 	public GameObject BulletObj;
 	private GameObject PlayerShip;
-	private static float shotTimer = 0.125f;
-	private float currentShotTimer;
 
 	// Use this for initialization
 	void Start () {
 		PlayerShip = transform.parent.parent.gameObject;
-		currentShotTimer = shotTimer;
-        BulletObj = Resources.Load<GameObject>("Prefabs/Bullet");
+		BulletObj = Resources.Load<GameObject>("Prefabs/Bullet");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			var shipVelocity = PlayerShip.GetComponent<PlayerMovement>().CurrentVelocity;
+			var bullet = (GameObject)Instantiate (BulletObj, gameObject.transform.position, gameObject.transform.rotation);
 
-		currentShotTimer -= Time.deltaTime;
-
-		if (currentShotTimer <= 0) {
-			if (Input.GetButton ("Fire1")) {
-				var shipVelocity = PlayerShip.GetComponent<PlayerMovement>().CurrentVelocity;
-				var bullet = (GameObject)Instantiate (BulletObj, gameObject.transform.position, gameObject.transform.rotation);
-
-				bullet.GetComponent<Bullet>().SetPlayerShipVelocity(shipVelocity);
-
-				currentShotTimer = shotTimer;
-			}
+			bullet.GetComponent<Bullet>().SetPlayerShipVelocity(shipVelocity);
+			var bulletCollider = bullet.GetComponent<Collider2D>();
+			var shipCollider = PlayerShip.GetComponent<Collider2D>();
+			Physics2D.IgnoreCollision(bulletCollider, shipCollider);
 		}
 	}
 }
