e339a5caf4f60a7c299c83056bbf0b513db6ec1f
Thomas Beirne <thomas.beirne@gmail.com>
2016-08-06 16:16:44 -0700

    Add minor comments

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -13,10 +13,13 @@ public class Shooting : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
-			var shipVelocity = PlayerShip.GetComponent<PlayerMovement>().CurrentVelocity;
 			var bullet = (GameObject)Instantiate (BulletObj, gameObject.transform.position, gameObject.transform.rotation);
 
+			// Give the bullet the ship's velocity so it goes faster if the player is moving
+			var shipVelocity = PlayerShip.GetComponent<PlayerMovement>().CurrentVelocity;
 			bullet.GetComponent<Bullet>().SetPlayerShipVelocity(shipVelocity);
+
+			// Don't allow bullets to collide with the player
 			var bulletCollider = bullet.GetComponent<Collider2D>();
 			var shipCollider = PlayerShip.GetComponent<Collider2D>();
 			Physics2D.IgnoreCollision(bulletCollider, shipCollider);
